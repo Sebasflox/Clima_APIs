efeffaee7e4bb0afdb0a609d341374e6205e5922
Sebasflox <sebas33115@gmail.com>
2022-02-26 16:16:49 -0600

    Create Codigo_Clima.js

---
+++ js/Codigo_Clima.js
@@ -0,0 +1,12 @@
+
+document.querySelector("#cambiar").onclick=function(){
+
+    let Contenido = document.querySelector('#contenido').value;
+
+    if (Contenido="San_Jose"){
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${10.64465243222477}&lon=${-85.42935960744244}&appid=$5d073f029a273b5dd7712679b2046c2e`
+    console.log(url)
+
+}
