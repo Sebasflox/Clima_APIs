8f9705adc73d1569b3cfb06d017c820b42fef0b4
GSALASB <gsalasb7@gmail.com>
2022-02-27 23:24:08 -0600

    Update custom.js

--- JS/custom.js
+++ JS/custom.js
@@ -1,7 +1,8 @@
 
 document.querySelector("#cambiar").onclick=function(){
+    document.getElementById("contenedorMapa").style.display="block"
     resfreshClima(document.querySelector("#provincia").value);
     refresMap(document.querySelector("#provincia").value);
-
+    ;
     
 }
