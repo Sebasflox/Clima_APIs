7c0ebc69f11255252032eb5337f168f75b0b51b1
Sebasflox <sebas33115@gmail.com>
2022-03-01 20:40:51 -0600

    Update Codigo_Clima.js

--- js/Codigo_Clima.js
+++ js/Codigo_Clima.js
@@ -1,35 +1,6 @@
 function resfreshClima(provincia){
 
-    if (provincia=="San_Jose"){
-        var lat = 9.938543933578808;
-        var long = -84.0969633937918;
-    }
-    if(provincia=="Alajuela"){
-        var lat = 10.015994926927956;
-        var long = -84.21445924689671;
-    }
-    if(provincia=="Cartago"){
-        var lat = 9.86302143845792;
-        var long = -83.91688551722636;
-    }
-    if(provincia=="Heredia"){
-        var lat = 10.473689889599;
-        var long = -84.01708618549425;
-    }
-    if(provincia=="Guanacaste"){
-        var lat = 10.629545059416854;
-        var long = -85.44593999484933;
-    }
-    if(provincia=="Puntarenas"){
-        var lat = 9.982056305799318;
-        var long = -84.83043792326303;
-    }
-    if(provincia=="Limon"){
-        var lat = 9.990157612958788;
-        var long = -83.04023875138913;
-    }
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=997ca64dfebf85c09099afb798ff1341";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+provincia+",cr&appid=997ca64dfebf85c09099afb798ff1341&lang=sp";
     
     $.getJSON(url, function(data) {
         console.log(data);
