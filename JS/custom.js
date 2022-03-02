a11a1928ee9523d9138fa9cc1988f1d6db576fe4
Sebasflox <sebas33115@gmail.com>
2022-03-01 20:40:42 -0600

    Update custom.js

--- JS/custom.js
+++ JS/custom.js
@@ -1,8 +1,6 @@
 
 document.querySelector("#cambiar").onclick=function(){
-    document.getElementById("contenedorMapa").style.display="block"
+    document.getElementById("contenedorMapa").style.display="block";
     resfreshClima(document.querySelector("#provincia").value);
     refresMap(document.querySelector("#provincia").value);
-    ;
-    
 }
